863e9f428f8a53c34dfa9b981e92f11a6f133512
shevvss <78696592+shevvss@users.noreply.github.com>
2022-06-21 18:38:45 +0400

    init

---
+++ app.js
@@ -0,0 +1,21 @@
+function slidesPlagin(activeSlide = 0) {
+  const slides = document.querySelectorAll('.slide');
+
+  slides[activeSlide].classList.add('active');
+
+  for (const slide of slides) {
+    slide.addEventListener('mouseover', () => {
+      clearActiveClasses();
+
+      slide.classList.add('active');
+    });
+  }
+
+  function clearActiveClasses() {
+    slides.forEach((slide) => {
+      slide.classList.remove('active');
+    });
+  }
+}
+
+slidesPlagin();
